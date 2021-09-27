a1066817e922c976ce2495bd6b094055a1285b0f
CarinaRoberta <silva.carina@live.com>
2021-09-27 18:15:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-anonymous-default-export */
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import "./App.css";
+import FeatureMovie from "./components/FeatureMovie";
+import Header from "./components/Header";
 
-function App() {
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //Pegando o filme em destaque (feature)
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+
+      setFeatureData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Header black={blackHeader} />
+      {featureData && <FeatureMovie item={featureData} />}
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+      <footer>
+        Feito em ReactJS
+        <br />
+        Direitos de imagem para Netflix
+        <br />
+        Dados pegos do site TheMovieDB
+      </footer>
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="http://cdn.playbuzz.com/cdn/9e9f12db-279e-4141-8fe6-fd5bfc5d9fb8/4f155204-7266-486d-88a5-2018ff11f947.gif"
+            alt="Carregando"
+          />
+        </div>
+      )}
     </div>
   );
-}
-
-export default App;
+};
